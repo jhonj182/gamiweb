1e0bc2f07707b762f969505619cd891a66af9414
jhonj182 <jtamayo182@gmail.com>
2024-08-24 16:30:35 -0500

    Add faq

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,41 +3,49 @@ document.addEventListener("DOMContentLoaded", function () {
   const priceText = document.querySelector('.js-feature-text');
   const checkbox = document.getElementById('switchSizeLargeChecked');
 
-  checkbox.addEventListener('change', function () {
-    if (checkbox.checked) {
-      // Mostrar precio con descuento
-      priceSpan.innerHTML = priceSpan.getAttribute('data-price-discount');
-      priceText.innerHTML = priceText.getAttribute('data-text');
-    } else {
-      // Mostrar precio sin descuento
-      priceSpan.innerHTML = priceSpan.getAttribute('data-price');
-      priceText.innerHTML = priceText.getAttribute('data-month');
-    }
-  });
-
-  var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 1.1, // Permite ver parte de la siguiente tarjeta
-    spaceBetween: 8, // Espacio entre tarjetas
-    centeredSlides: true,
-    grabCursor: true,
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true, // Permite que los dots sean clicables
-      dynamicBullets: true // Hace que los dots sean más pequeños o más grandes según la posición
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 2, // Desactiva el slider en pantallas más grandes
-        centeredSlides: false,
+  if (priceSpan && priceText && checkbox) {
+    checkbox.addEventListener('change', function () {
+      if (checkbox.checked) {
+        // Mostrar precio con descuento
+        priceSpan.innerHTML = priceSpan.getAttribute('data-price-discount');
+        priceText.innerHTML = priceText.getAttribute('data-text');
+      } else {
+        // Mostrar precio sin descuento
+        priceSpan.innerHTML = priceSpan.getAttribute('data-price');
+        priceText.innerHTML = priceText.getAttribute('data-month');
       }
-    }
-  });
-  document.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    if (window.scrollY > 20) {
-      header.classList.add("sticky");
-    } else {
-      header.classList.remove("sticky");
-    }
-  });
-})
+    });
+  }
+
+  const swiperContainer = document.querySelector('.swiper-container');
+  if (swiperContainer) {
+    var swiper = new Swiper(swiperContainer, {
+      slidesPerView: 1.1, // Permite ver parte de la siguiente tarjeta
+      spaceBetween: 8, // Espacio entre tarjetas
+      centeredSlides: true,
+      grabCursor: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true, // Permite que los dots sean clicables
+        dynamicBullets: true // Hace que los dots sean más pequeños o más grandes según la posición
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2, // Desactiva el slider en pantallas más grandes
+          centeredSlides: false,
+        }
+      }
+    });
+  }
+
+  const header = document.querySelector(".header");
+  if (header) {
+    document.addEventListener("scroll", function () {
+      if (window.scrollY > 20) {
+        header.classList.add("sticky");
+      } else {
+        header.classList.remove("sticky");
+      }
+    });
+  }
+});
